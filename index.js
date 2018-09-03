e55339c2c5f8a1d6799d096d1e52645560b2bdd3
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-03 08:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {key : "value"}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({recipes}, object, {[key] : value})
 }
+
+function destructivelyUpdateObjectWithAndValue(object, key, value){
+  object[key] = "value"
+}
