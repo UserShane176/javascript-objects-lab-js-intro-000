86a7c0604af02ece79a52913d96f7eb87b71041c
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-03 08:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithAndValue(object, key, value){
   object[key] = "value"
 }
+
+function deleteFromObjectByKey(object, key){
+  delete object[key] 
+  return object
+}
